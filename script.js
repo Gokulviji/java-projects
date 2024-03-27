76d85892d72e46b97c4d5ff9cd4c29a669e3f37c
gokul859305 <149158756+Gokulviji@users.noreply.github.com>
2024-03-27 15:55:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+
+function opening(){
+    let name = prompt("please enter your name");
+    let mark = prompt("please enter your mark");
+    if(mark>=50 && mark<=70){
+
+        document.getElementById("output").innerHTML= name+" "+"you are pass";
+
+    } else if( mark>70 && mark<100){
+
+        document.getElementById("output").innerHTML= name+" "+"you are exxelent";
+    }
+    else{
+        document.getElementById("output").innerHTML= name+" "+"you are fail";
+    }
+} 
+
+
+
+
+function fit(){
+    let age=prompt("please enter your age");
+    let height=prompt("please enter your height");
+    let weight=prompt("please enter your weight");
+    if(age>18 && height>6 && weight>50){
+        document.getElementById("outpit").innerHTML= "you are eligeble";
+    }else{
+        document.getElementById("outpit").innerHTML= "you are not eligeble";
+    }
+}
+
+
+
+
+
+function prime(){
+    let number = prompt("Enter your number");
+    let isPrime = true;
+
+    if(number == 1){
+        document.getElementById("outpet").innerHTML="1 is prime nor composite number."
+    }
+    else if( number>1){
+           for( let i=2;i<number/2;i++){
+            if(number % i==0 ){
+                isPrime=false;
+                break;
+            }
+           }
+           if(isPrime==true){
+               document.getElementById("outpet").innerHTML="your number is prime"
+           }else{
+            document.getElementById("outpet").innerHTML="your number is not a prime"
+           }
+    }
+    else{
+        document.getElementById("outpet").innerHTML="your number is not a prime"
+    }
+}
+
